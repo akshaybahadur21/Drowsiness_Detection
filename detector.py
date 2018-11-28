550328da5a64d718a726bfbdfb596cfa8b656d57
Rithik Yelisetty <rithik@gmail.com>
2018-11-28 12:38:01 -0500

    Try OpenCV tutorial to see if its faster

--- detector.py
+++ detector.py
@@ -1,4 +1,4 @@
-import dlib
+# import dlib
 import cv2
 from scipy.spatial import distance
 from imutils import face_utils
@@ -13,30 +13,22 @@ class Detector():
         self.tEyesNotVisible = eyesNotVisibleTime
         self.tFrame_check_time = frame_check_time
 
-        (self.lStart,self.lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
-        (self.rStart,self.rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
-
         self.thresh = 0.25
         self.frame_check = 20
-        self.detect = dlib.get_frontal_face_detector()
-        self.predict = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-
-    def eye_aspect_ratio(self, eye):
-        A = distance.euclidean(eye[1], eye[5])
-        B = distance.euclidean(eye[2], eye[4])
-        C = distance.euclidean(eye[0], eye[3])
-        ear = (A + B) / (2.0 * C)
-        return ear
+        self.eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
     def isDistracted(self, frame, drawing):
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        subjects = self.detect(gray, 0)
-        if len(subjects) == 0:
-            self.eyesNotVisible+=1
-            if self.eyesNotVisible >= self.tEyesNotVisible: # Distracted checker
-                return True
-        else:
-            self.eyesNotVisible = 0
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        eyes = eye_cascade.detectMultiScale(gray)
+        print(len(eyes))
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # subjects = self.detect(gray, 0)
+        # if len(subjects) == 0:
+        #     self.eyesNotVisible+=1
+        #     if self.eyesNotVisible >= self.tEyesNotVisible: # Distracted checker
+        #         return True
+        # else:
+        #     self.eyesNotVisible = 0
         return False
 
     def display_warnings(self, frame):
